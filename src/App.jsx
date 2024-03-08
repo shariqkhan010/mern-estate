05cac564ad8fcad843e58e74932858a338e5928e
Shariq Khan <shariqkhanns@gmail.com>
2024-03-09 02:35:30 +0500

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,22 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Sign from './pages/Sign';
 import SignU from './pages/SignU';
+import Header from './components/Header';
 
 
 
-export default function App() {``
-  return <BrowserRouter>
-  <Routes>
-  <Route path='/home' element={<Home />} />
-  <Route path='/about' element={<About />} />
-  <Route path='/Profile' element={<Profile />} />
-  <Route path='/Sign' element={<Sign />} />
-  <Route path='/SignU' element={<SignU />} />
-
-  </Routes>
-  </BrowserRouter>;
-}
+export default function App() {
+  return (
+  <BrowserRouter>
+  <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/Profile' element={<Profile />} />
+      <Route path='/Sign' element={<Sign />} />
+      <Route path='/SignU' element={<SignU />} />
+    </Routes>
+  </BrowserRouter>
+)}
 
 
